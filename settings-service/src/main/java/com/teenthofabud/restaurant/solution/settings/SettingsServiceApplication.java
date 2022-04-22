eed4b24645ddf5a16b2c726719e44322a8b7ca6b
SayanDey92 <38313671+SayanDey92@users.noreply.github.com>
2022-04-23 00:33:50 +0530

    Added the configuration of Eureka client in all the services. Added parameters for dynamic insertion from run.bat file.

--- settings-service/src/main/java/com/teenthofabud/restaurant/solution/settings/SettingsServiceApplication.java
+++ settings-service/src/main/java/com/teenthofabud/restaurant/solution/settings/SettingsServiceApplication.java
@@ -3,8 +3,10 @@ package com.teenthofabud.restaurant.solution.settings;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication(exclude = { UserDetailsServiceAutoConfiguration.class })
+@EnableEurekaClient
 public class SettingsServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(SettingsServiceApplication.class);
